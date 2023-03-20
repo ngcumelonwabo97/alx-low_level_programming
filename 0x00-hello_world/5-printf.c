8b8f53a2622ad2cefc4413877643d7a4ff6a1ed1
Lonwabo <lonwabomshudulu@gmail.com>
2023-03-20 14:37:23 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main fuction
+ *
+ * REturn : 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return 0;
+}
