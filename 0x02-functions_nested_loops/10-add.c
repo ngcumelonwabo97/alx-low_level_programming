f55fa69f412da0c475d3a67a38196acca38abc75
Lonwabo <lonwabomshudulu@gmail.com>
2023-03-23 21:46:00 +0000

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - the function adds two integers and returns the result
+ *
+ * @a: function parameter
+ * @b: function parameter
+ *
+ * Return: c
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
