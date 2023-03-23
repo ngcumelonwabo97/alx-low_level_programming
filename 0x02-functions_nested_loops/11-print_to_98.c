0a180e2afa05e5a493a921fcb8e5623056853c40
Lonwabo <lonwabomshudulu@gmail.com>
2023-03-23 22:42:04 +0000

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,14 @@
  */
 
 void print_to_98(int n)
-i
+{
 	int i;
 
 	if (n >= 0 && n <= 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
 			if (i != 98)
 				printf(", ");
 		}
@@ -27,7 +27,7 @@ i
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
 			if (i != 98)
 				printf(", ");
 		}
@@ -35,10 +35,9 @@ i
 	}
 	else if (n > 98)
 	{
-		for (i = n; i > 98; i--)
+		for (i = n; i <= 98; i--)
 		{
-			printf("%d, ", i);
-			printf(", ");
+			printf("%d", i);
 		}
 		printf("\n");
 	}
