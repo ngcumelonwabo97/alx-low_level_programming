25faa12ca8f7880cceda584c3d0b0275430a00bc
Lonwabo <lonwabomshudulu@gmail.com>
2023-03-29 01:19:27 +0000

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - checks the length of the string
+ * @s: string to be checked
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length])
+	{
+		length++;
+	}
+
+	return (length);
+}
