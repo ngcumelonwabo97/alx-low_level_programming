d552b607f5ef3d3598d90588ee55c780fa1c0508
Lonwabo <ngcumelonwabo97@gmail.com>
2023-06-19 15:07:41 -0700

    0x01. Variables, if, else, while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main - a program that prints the alphabet in lowercase, followed by a new line.
+ * main - a program that prints the alphabet in lowercase, 
+ * followed by a new line.
  *
  * Return: 0 success
  */
