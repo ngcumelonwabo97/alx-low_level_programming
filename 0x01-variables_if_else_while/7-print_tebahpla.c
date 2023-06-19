6c4af015b40068ed3c64874de3fca0bc95ee708e
Lonwabo <ngcumelonwabo97@gmail.com>
2023-06-19 15:30:56 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,7 +2,7 @@
 /**
  * main - a program that prints the lowercase alphabet in reverse,
  * followed by a new line.
- * You can only use the putchar function 
+ * You can only use the putchar function
  * (every other function (printf, puts, etc…) is forbidden).
  *
  * Return: 0 success
