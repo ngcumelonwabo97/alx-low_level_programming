d8d799e54bcba485ce03506bc73557a78ff37905
Lonwabo <ngcumelonwabo97@gmail.com>
2023-06-21 17:41:38 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * void jack_bauer(void) - a function that prints every minute of the day
+ * jack_bauer - a function that prints every minute of the day
  * of Jack Bauer, starting from 00:00 to 23:59.
  *
  * Return: 0 (success)
