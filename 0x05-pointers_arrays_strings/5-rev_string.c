f26a469661b5fa96a67957e6d69be247536e8cee
Lonwabo <ngcumelonwabo97@gmail.com>
2023-06-26 18:02:12 -0700

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - a function that reverses a string.
+ * @s: char variable
+ * Return: string in reverse
+ */
+
+void rev_string(char *s)
+{
+	int counter = 0;
+	int i;
+	char rev = s[0];
+
+	while (s[counter] != '0')
+	{
+		counter++;
+	}
+
+	for (i = 0; i < counter; i++)
+	{
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
+	}
+}
