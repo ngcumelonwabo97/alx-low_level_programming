2e7eb5eef14dbc717ebc4c3a1668f8c3a101fa5d
Lonwabo <lonwabomshudulu@gmail.com>
2023-06-29 23:56:49 +0000

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,6 +1,32 @@
 #include "main.h"
 
 /**
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
+ */
+
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
  * infinite_add - a function that adds two numbers.
  * @n1: first number to be inserted
  * @n2: second number to be inserted
@@ -8,19 +34,13 @@
  * @size_r: buffer size
  * Return: pointer to calling function
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int overflow = 0;
-	int digits = 0;
-	int val1 = 0;
-	int temp_total = 0;
-	int val2 = 0;
-	int i = 0, j = 0;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
 	while (*(n1 + i) != '\0')
 		i++;
-
 	while (*(n2 + j) != '\0')
 		j++;
 	i--;
@@ -28,27 +48,32 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	if (j >= size_r || i >= size_r)
 		return (0);
-
 	while (j >= 0 || i >= 0 || overflow == 1)
 	{
 		if (i < 0)
 			val1 = 0;
 		else
-			val1 = *(n1 + i) - '\0';
+			val1 = *(n1 + i) - '0';
 		if (j < 0)
 			val2 = 0;
 		else
-			val2 = *(n2 + j) - '\0';
-		temp_total = val1 + val2 + overflow;
-		if (temp_total >= 10)
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
 			overflow = 1;
 		else
 			overflow = 0;
 		if (digits >= (size_r - 1))
 			return (0);
-		*(r + digits) = (temp_total % 10) + '0';
+		*(r + digits) = (temp_tot % 10) + '0';
 		digits++;
 		j--;
 		i--;
 	}
+	if (digits == size_r)
+	return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
 }
+
