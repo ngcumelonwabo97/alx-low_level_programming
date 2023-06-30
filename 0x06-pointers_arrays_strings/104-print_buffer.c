892414c3304a266ac00e123f1dc4bb6a9ef98c0e
Lonwabo <lonwabomshudulu@gmail.com>
2023-06-30 00:28:53 +0000

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,7 +10,7 @@
 
 void print_buffer(char *b, int size)
 {
-	int i, j, o;
+	int o, j, i;
 
 	o = 0;
 
@@ -19,29 +19,29 @@ void print_buffer(char *b, int size)
 		printf("\n");
 		return;
 	}
-
 	while (o < size)
 	{
 		j = size - o < 10 ? size - o : 10;
-		printf("%08x:", o);
-
+		printf("%08x: ", o);
 		for (i = 0; i < 10; i++)
 		{
 			if (i < j)
 				printf("%02x", *(b + o + i));
 			else
-				printf(" ");
-
+				printf("  ");
 			if (i % 2)
+			{
 				printf(" ");
+			}
 		}
-
 		for (i = 0; i < j; i++)
 		{
 			int c = *(b + o + i);
 
 			if (c < 32 || c > 132)
+			{
 				c = '.';
+			}
 			printf("%c", c);
 		}
 		printf("\n");
