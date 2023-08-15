1a83e395a9cce31d4e6ab44ec5a9762a736ff9d1
Lonwabo <Lonwabomshudulu@gmail.com>
2023-08-15 22:43:59 +0000

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,12 +19,12 @@ struct dog
 /**
  * poppy - typedef for struct dog
  */
-typedef struct dog poppy;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-poppy *new_dog(char *name, float age, char *owner);
-void free_dog(poppy *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
