67489aae008643fb0feafdb1c78a85e70ca0ec17
Lonwabo <Lonwabomshudulu@gmail.com>
2023-08-15 14:19:39 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,31 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new type struct dog
+ * with its basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * poppy - typedef for struct dog
+ */
+typedef struct dog poppy;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+poppy *new_dog(char *name, float age, char *owner);
+void free_dog(poppy *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
